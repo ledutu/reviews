d585891b3fceb68678be3bcb567ec030a7af0cf1
ledutu <ledutu@gmail.com>
2021-06-30 23:22:25 +0700

    update

--- backend-api/app.js
+++ backend-api/app.js
@@ -3,32 +3,45 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./src/routes');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'ejs');
+
+// express-session
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/src/public')));
+app.use('/axios', express.static(path.join(__dirname, 'node_modules', 'axios')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//Router
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ backend-api/src/routes/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+// router.use('/util', utils);
+// router.use('/user', user);
+
+module.exports = router;
