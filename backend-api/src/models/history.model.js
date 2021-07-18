d9326f48a87b3f121d62772c459dab9381250b34
ledutu <ledutu@gmail.com>
2021-07-18 17:43:20 +0700

    update history action

--- backend-api/src/models/history.model.js
+++ backend-api/src/models/history.model.js
@@ -2,10 +2,25 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const historySchema = new Schema({
-    action: { type: Schema.Types.ObjectId, ref: 'history_actions' },
+    user: { type: Schema.Types.ObjectId, ref: 'users' },
+    action_name: { type: String, default: '' },
     comment: { type: String, require: true },
     is_hide: { type: Boolean, default: false },
 }, { timestamps: { currentTime: () => Date.now() } });
+
+historySchema.statics.saveHistory = async function (userId, actionName, comment) {
+    try {
+        history = new History({
+            user: userId,
+            action_name: actionName,
+            comment,
+        });
+        
+        await history.save();
+    } catch (error) {
+        throw new Error();
+    }
+}
 
 const History = mongoose.model('histories', historySchema);
 
