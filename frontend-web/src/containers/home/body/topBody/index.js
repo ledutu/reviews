d3664ab3d333a3d88fa42966d5d91d9840974edf
caolinh3561 <caobalinh3561@gmail.com>
2021-07-20 13:15:26 +0700

    update get data from API

--- frontend-web/src/containers/home/body/topBody/index.js
+++ frontend-web/src/containers/home/body/topBody/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./TopBody.module.scss";
+import Link from "next/link";
+import { pushPostToStore } from "../../../../store/actions/postAction";
+import { useDispatch } from "react-redux";
 function TopContent(props) {
+  const dispatch = useDispatch();
   let posts = props.posts.posts;
-  console.log(posts);
   if (!posts) return null;
   const renderSubContent = () => {
     var arr = [];
@@ -16,32 +19,61 @@ function TopContent(props) {
     return arr.map((item) => {
       return (
         <div key={item._id}>
-          <div className={styles.wrapperImage}>
-            <Image
-              className={styles.imageCover}
-              src={item.image}
-              width="300"
-              height="160"
-              alt="newestContent"
-            />
-          </div>
+          <Link
+            href={{
+              pathname: `/post`,
+              query: { id: item._id },
+            }}
+          >
+            <div
+              className={styles.wrapperImage}
+              onClick={() => {
+                handleClickLink(item);
+              }}
+            >
+              <Image
+                className={styles.imageCover}
+                src={item.image}
+                width="300"
+                height="160"
+                alt="newestContent"
+              />
+            </div>
+          </Link>
           <h3 className={styles.title}>{item.title}</h3>
         </div>
       );
     });
   };
 
+  const handleClickLink = (item) => {
+    dispatch(pushPostToStore(item));
+  };
+
   return (
     <div id="topContent" className={styles.topContent}>
       <div className={styles.bigContent}>
         <div className={styles.wrapper}>
-          <Image
-            className={styles.imageCover}
-            src={posts[0].image}
-            width="720"
-            height="405"
-            alt="newestContent"
-          />
+          <Link
+            href={{
+              pathname: `/post`,
+              query: { id: posts[0]._id },
+            }}
+          >
+            <div
+              onClick={() => {
+                handleClickLink(posts[0]);
+              }}
+            >
+              <Image
+                className={styles.imageCover}
+                src={posts[0].image}
+                width="720"
+                height="405"
+                alt="newestContent"
+              />
+            </div>
+          </Link>
           <div className={styles.bigContentTitle}>
             <h2 className={styles.title}>{posts[0].title}</h2>
           </div>
